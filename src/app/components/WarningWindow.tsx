0b08686208794393e75089f78bb0d914face95bf
Taylor Hutchens <taylor@thirdlocust.com>
2026-04-17 08:08:43 -0600

    bum node versions

--- src/app/components/WarningWindow.tsx
+++ src/app/components/WarningWindow.tsx
@@ -16,7 +16,7 @@ const WarningWindow: React.FC<WarningWindowProps> = ({ isVisible, onRestore, onD
         </div>
         <div className="mac-content warning-content">
             <span className="warning-icon">⚠️</span>
-            <h3>You have deleted all of Taylor's work!</h3>
+            <h3>You have deleted all of Taylor&apos;s work!</h3>
             <p>All windows have been closed. Did you mean to do that?</p>
             <div className="warning-actions">
                 <button onClick={onDismiss}>Dismiss</button>
